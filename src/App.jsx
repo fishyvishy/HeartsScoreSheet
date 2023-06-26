e225ae7d142de4a2b3e76889c6a7680100fdb14b
Vishwanath Wimalasena <vishy235@gmail.com>
2023-06-26 01:36:39 -0400

    testing out css animations

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,20 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  const [gameStarted, setGameStarted] = useState(false);
+
+  function startGame() {
+    setGameStarted(true);
+  }
+
   return (
     <>
       <div className="titleBar">
-        <img src={heartslogo} />
+        <img src={heartslogo} className={gameStarted ? "movingImg" : ""} />
         <h1>Hearts Scoresheet</h1>
       </div>
       <div className="button-panel">
-        <button>New Game</button>
+        <button onClick={startGame}>New Game</button>
         <a href="https://bicyclecards.com/how-to-play/hearts/" target="_blank">
           <button>Rules</button>
         </a>
