825f0eec415c8cdc8131af9699ac1c395ec5c091
Vishwanath Wimalasena <vishy235@gmail.com>
2023-06-28 19:18:16 -0600

    shake animation added to modal

--- src/Components/AddRoundModal.jsx
+++ src/Components/AddRoundModal.jsx
@@ -3,43 +3,74 @@ import { useState } from "react";
 export default function AddRoundModal(props) {
   const { names, toggle, scoreUpdater } = props;
   const [playerPoints, setPlayerPoints] = useState([0, 0, 0, 0]);
+  const [shake, setShake] = useState(false);
 
-  const formInputs = names.map((name) => (
-    <div key={name}>
-      <label>{name}</label>
-      <input
-        type="number"
-        min="0"
-        max="26"
-        value={playerPoints[names.indexOf(name)]}
-        onChange={(e) => handleChange(names.indexOf(name), e)}
-      />
-    </div>
-  ));
+  const formInputs = names.map((name) => {
+    const index = names.indexOf(name);
+    const val = playerPoints[index].toString();
+    return (
+      <div key={name} className="modal-input">
+        <label htmlFor={name}>{name}</label>
+        <input
+          type="number"
+          min="0"
+          max="26"
+          value={val}
+          id={name}
+          onChange={(e) => handleChange(index, e)}
+        />
+      </div>
+    );
+  });
 
   function handleChange(index, event) {
     setPlayerPoints((prevPoints) => {
       let updatedPoints = [...prevPoints];
-      updatedPoints[index] = Number(event.target.value);
+      const val = event.target.value;
+      updatedPoints[index] = Number(val);
       return updatedPoints;
     });
   }
 
-  function submitPoints() {
-    scoreUpdater(playerPoints);
-    toggle();
+  function submitPoints(event) {
+    event.preventDefault();
+    const inRange = playerPoints.every((num) => num === 26 || num === 0);
+    const sum = playerPoints.reduce(
+      (accumulator, runner) => (runner += accumulator),
+      0
+    );
+    if (sum === 26) {
+      const qSpadePlayed = playerPoints.some((num) => num >= 13);
+      if (!qSpadePlayed) {
+        console.log("queen of spades must be played");
+        setShake(true);
+      } else {
+        scoreUpdater(playerPoints);
+        toggle();
+      }
+    } else {
+      if (inRange && sum === 78) {
+        console.log("someone shot the moon");
+      } else {
+        setShake(true);
+      }
+    }
   }
 
   return (
-    <div className="add-round-modal">
-      <h1>Enter Scores</h1>
-      <form className="add-round-modal-form">
-        {formInputs}
-        <div>
-          <button onClick={submitPoints}>Add Points</button>
-          <button onClick={toggle}>Cancel</button>
+    <>
+      <div className="modal-container">
+        <div className={`add-round-modal ${shake ? "modal-shake" : ""}`}>
+          <h1>Enter Scores</h1>
+          <form className="add-round-modal-form">
+            {formInputs}
+            <div>
+              <button onClick={submitPoints}>Add Points</button>
+              <button onClick={toggle}>Cancel</button>
+            </div>
+          </form>
         </div>
-      </form>
-    </div>
+      </div>
+    </>
   );
 }
